2b7de3550dcacc62c656b2c7dee35efd0c817094
feruap <feruap@gmail.com>
2026-03-14 16:15:03 -0600

    chore: remove 66 debug/test/temp orphan files
    Cleaning project structure - removed all test_*, check_*, *.log, dump*.json
    and other development artifacts that are not part of the app.

--- apps/server/check_camp_cols.js
+++
@@ -1,5 +0,0 @@
-const { Pool } = require('pg');
-const p = new Pool({ connectionString: 'postgres://postgres:postgres@localhost:5432/myalice_clone' });
-p.query("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'campaigns'")
-    .then(r => console.log(JSON.stringify(r.rows, null, 2)))
-    .finally(() => p.end());
--- apps/server/get_schema.js
+++
@@ -1,6 +0,0 @@
-const { Pool } = require('pg');
-const fs = require('fs');
-const p = new Pool({ connectionString: 'postgres://postgres:postgres@localhost:5432/myalice_clone' });
-p.query("SELECT table_name, column_name, data_type FROM information_schema.columns WHERE table_schema = 'public' and table_name IN ('assignment_rules','teams','channels')")
-    .then(r => fs.writeFileSync('schema_output.json', JSON.stringify(r.rows, null, 2)))
-    .finally(() => p.end());
